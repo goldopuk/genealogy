a5e66ca4f6b66048e5a50d5ed4f82ff890c1f364
Julien Colomb <juliencolomb@gmail.com>
2015-05-26 14:25:46 -0300

    text intro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,14 @@
 @section('content')
 
 	<p class="intro">
-		Não sou muito conhecedor em Laravel. Por isso, não estou usando todos os recursos dele, como Service Provider/Container, flash message, etc...
+		Hey, Mobly, <br>
+		Não sou muito conhecedor em Laravel e não estou usando todos os recursos dele, como Blade, Service Provider/Container, flash message, etc...<br>
+		Tambêm, não tenho muito tempo e estou fazendo o desenvolvimento ás pressas. <br>
+		Pode ser que tem falhas de segurança aos Xss e CSRF attacks.
+		Mas tentei mostrar minha capacidade a trabalhar tanto com o frontend quanto
+		com o backend, e implementei scripts para instalar o app, executar os unit tests (limitados...), limpar e ajudar o desenvolvimento desse projeto.<br>
+		Inclui angular, bower, compass, composer, build file, scripts de instalação. <br>
+		Desculpa pela interface, não esta bonita não e a navigação pouco amigavel... :)<br>
 	</p>
 
 	<div class="main categories">
@@ -20,5 +27,3 @@
 	</div>
 	
 @stop
-
-   
